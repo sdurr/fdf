7b72664e1a7dcb6e04894dbe60679093448dbd20
sdurr <sdurr@e3r1p12.42.fr>
2015-02-17 14:22:03 +0100

    map pixel aff

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: sdurr <sdurr@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/16 19:18:05 by sdurr             #+#    #+#             */
-/*   Updated: 2015/02/17 12:03:50 by sdurr            ###   ########.fr       */
+/*   Updated: 2015/02/17 13:52:43 by sdurr            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int		main(int ac, char **av)
 	if (ac == 2)
 		e.map = av[1];
 	e.mlx = mlx_init();
-	e.window = mlx_new_window(e.mlx, 420, 420, "fdf");
+	e.window = mlx_new_window(e.mlx, 800, 800, "fdf");
 	mlx_expose_hook(e.window, expose_hook, &e);
 	mlx_key_hook(e.window, key_hook, &e);
 	mlx_mouse_hook(e.window, mouse_hook, &e);
--- windows.c
+++ windows.c
@@ -6,12 +6,13 @@
 /*   By: sdurr <sdurr@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/17 11:22:13 by sdurr             #+#    #+#             */
-/*   Updated: 2015/02/17 12:13:42 by sdurr            ###   ########.fr       */
+/*   Updated: 2015/02/17 14:21:36 by sdurr            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <mlx.h>
 #include <fcntl.h>
+#include <stdio.h>
 #include <unistd.h>
 #include "fdf.h"
 #include "libft.h"
@@ -24,17 +25,34 @@ void		windows(void *mlx, void *window, char *map)
 	int		fd;
 	int		ret;
 	char	*line;
+	char *recup;
+	int i;
 
 	mlx = mlx;
 	window = window;
-	x = 0;
-	y = 0;
-	x = x;
-	y = y;
 	ret = 1;
 	fd = open(map, O_RDONLY);
+	x = 200;
+	y = 200;
+	line = ft_strnew(15);
 	while (ret == 1)
 	{
 		ret = get_next_line(fd, &line);
+		while (*line != '\0')
+		{
+			recup = ft_strnew(15);
+			i = 0;
+			while (*line != ' ')
+				recup[i] = *line++;
+			y += ft_atoi(recup);
+			mlx_pixel_put(mlx, window, x, y, 0xFF0000);
+			mlx_pixel_put(mlx, window, x, y, 0xFF0000);
+			y -= ft_atoi(recup);
+			line++;
+			x += 20;
+		}
+		line = ft_strnew(15);
+		y += 20;
+		x = 200;
 	}
 }
