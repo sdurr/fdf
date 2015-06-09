965841440e1c2e0e5662f2dc076f44967b031d4c
sdurr <sdurr@e3r1p12.42.fr>
2015-06-09 13:47:51 +0200

    add norm ++

--- expose.c
+++ expose.c
@@ -6,7 +6,7 @@
 /*   By: sdurr <sdurr@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/06/04 11:28:28 by sdurr             #+#    #+#             */
-/*   Updated: 2015/06/09 13:41:27 by sdurr            ###   ########.fr       */
+/*   Updated: 2015/06/09 13:47:19 by sdurr            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,20 @@
 #include "libft.h"
 #include <mlx.h>
 #include <stdlib.h>
+
+void		loop(t_bressen *b)
+{
+	if (b->e2 > -b->dx)
+	{
+		b->err -= b->dy;
+		b->x0 += b->sx;
+	}
+	if (b->e2 < b->dy)
+	{
+		b->err += b->dx;
+		b->y0 += b->sy;
+	}
+}
 
 void		bresenham(int x1, int y1, t_env *env)
 {
@@ -29,30 +43,22 @@ void		bresenham(int x1, int y1, t_env *env)
 	if (b.dy < 0)
 		b.dy = -b.dy;
 	b.ince = 2 * b.dy;
-	b.incne = 2 *(b.dy - b.dx);
+	b.incne = 2 * (b.dy - b.dx);
 	b.x = b.x0;
 	b.sx = b.x0 < x1 ? 1 : -1;
 	b.sy = b.y0 < y1 ? 1 : -1;
-	b.err = (b.dx > b.dy ? b.dx : -b.dy) /2;
+	b.err = (b.dx > b.dy ? b.dx : -b.dy) / 2;
 	while (1)
 	{
 		mlx_pixel_put(env->mlx, env->win, b.x0, b.y0, 0xFFFFFF);
 		b.e2 = b.err;
 		if (b.x0 == x1 && b.y0 == y1)
-			break;
-		if (b.e2 > -b.dx)
-		{
-			b.err -= b.dy;
-			b.x0 += b.sx;
-		}
-		if (b.e2 < b.dy)
-		{
-			b.err += b.dx;
-			b.y0 += b.sy;
-		}
+			break ;
+		loop(&b);
 	}
 }
-void	init_tab(t_env *e, int i, int j)
+
+void		init_tab(t_env *e, int i, int j)
 {
 	int width;
 	int coeff;
@@ -66,25 +72,23 @@ void	init_tab(t_env *e, int i, int j)
 	i = 0;
 	y = 30;
 	width = e->width;
-	while (height)
+	while (height--)
 	{
 		j = 0;
 		width = e->width;
 		e->tab_x[i] = (int *)malloc(sizeof(int) * width + 1);
 		e->tab_y[i] = (int *)malloc(sizeof(int) * width + 1);
 		x = 30;
-		while (width)
+		while (width--)
 		{
 			coeff = 3;
-			e->tab_x[i][j] = (int)(((0.07 *(float)x - 0.07 * (float) y)) * coeff + 370);
+			e->tab_x[i][j] = (int)(((0.07 * (float)x - 0.07 * (float)y)) * coeff + 370);
 			e->tab_y[i][j] = (int)(((0.04 * (float)x + 0.04 * (float)y) - 0.2 *(float)(e->map[i][j])) * coeff + 370);
 			x += 100;
-			width--;
 			j++;
 		}
 		y+=100;
 		i++;
-		height--;
 	}
 	e->tab_x[i] = NULL;
 	e->tab_y[i] = NULL;
--- fdf.h
+++ fdf.h
@@ -6,7 +6,7 @@
 /*   By: sdurr <sdurr@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/06/04 09:47:06 by sdurr             #+#    #+#             */
-/*   Updated: 2015/06/09 13:25:22 by sdurr            ###   ########.fr       */
+/*   Updated: 2015/06/09 13:39:25 by sdurr            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,8 +24,25 @@ typedef struct s_env
 	int		height;
 	int		**tab_x;
 	int		**tab_y;
-
+	int		x;
+	int		y;
 }				t_env;
+
+typedef struct s_bressen
+{
+	int x;
+	int y;
+	int x0;
+	int y0;
+	int dx;
+	int dy;
+	int ince;
+	int incne;
+	int sx;
+	int sy;
+	int err;
+	int e2;
+}			t_bressen;
 
 int			expose_hook(t_env *e);
 int			key_hook(int keycode, t_env *e);
